cbf9e7c009e3e273b8e3ce93067163432827f3f2
unTsar <35622885+unTsar@users.noreply.github.com>
2022-03-16 16:25:25 +0300

    1

--- app/js/app.js
+++ app/js/app.js
@@ -5,16 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
 	
-	var button = document.querySelector("#header_menuBtn");
-	var MenuDrawer = document.querySelector("#MenuDrawer_main");
 	
-	button.addEventListener("click", () => {
-		button.classList.toggle("is-active");
-		MenuDrawer.classList.toggle("MenuDrawer_open");
-
-	});
-
-	   
 	   
 
 });
